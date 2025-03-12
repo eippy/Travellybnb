7ad29a5104e6e31b393d176ff87c2e8d81b0cebb
eippy <michaelekim11@gmail.com>
2025-03-12 18:18:12 -0500

    Nest Login And Signup Buttons In Dropdown Menu

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -2,9 +2,10 @@ import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton';
+import OpenModalMenuItem from './OpenModalMenuItem';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
+import "./ProfileButton.css"
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
@@ -30,9 +31,12 @@ function ProfileButton({ user }) {
         return () => document.removeEventListener('click', closeMenu);
     }, [showMenu]);
 
+    const closeMenu = () => setShowMenu(false);
+
     const logout = e => {
         e.preventDefault();
         dispatch(sessionActions.logout());
+        closeMenu();
     };
 
     const ulClassName = 'profile-dropdown' + (showMenu ? '' : ' hidden');
@@ -56,18 +60,16 @@ function ProfileButton({ user }) {
                     </>
                 ) : (
                     <>
-                        <li>
-                            <OpenModalButton
-                                buttonText="Log In"
-                                modalComponent={<LoginFormModal />}
-                            />
-                        </li>
-                        <li>
-                            <OpenModalButton
-                                buttonText="Sign Up"
-                                modalComponent={<SignupFormModal />}
-                            />
-                        </li>
+                        <OpenModalMenuItem
+                            itemText="Log In"
+                            onItemClick={closeMenu}
+                            modalComponent={<LoginFormModal />}
+                        />
+                        <OpenModalMenuItem
+                            itemText="Sign Up"
+                            onItemClick={closeMenu}
+                            modalComponent={<SignupFormModal />}
+                        />
                     </>
                 )}
             </ul>
