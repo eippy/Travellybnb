74b942703ea77ef0b29034696d4ba0018f69356a
eippy <michaelekim11@gmail.com>
2025-03-12 18:06:24 -0500

    added signup modal and login modal/ updated profile button

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -6,27 +6,16 @@ import './Navigation.css';
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
 
-    const sessionLinks = sessionUser ? (
-        <li>
-            <ProfileButton user={sessionUser} />
-        </li>
-    ) : (
-        <>
-            <li>
-                <NavLink to="/login">Log In</NavLink>
-            </li>
-            <li>
-                <NavLink to="/signup">Sign Up</NavLink>
-            </li>
-        </>
-    );
-
     return (
         <ul>
             <li>
                 <NavLink to="/">Home</NavLink>
             </li>
-            {isLoaded && sessionLinks}
+            {isLoaded && (
+                <li>
+                    <ProfileButton user={sessionUser} />
+                </li>
+            )}
         </ul>
     );
 }
